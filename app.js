a1b6aec546623e6ce683115ac13d7a8bfa6c86ec
Matheus <matheus.hpp135@gmail.com>
2024-02-20 00:25:01 -0300

    feat: 🎸 routing requests

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  const url = req.url
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send message</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
   res.write('<head><title>My First Page</title></head>')
