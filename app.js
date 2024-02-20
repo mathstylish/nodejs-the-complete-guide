d5dd45c85995441bd31e91b5eef9050ea21a9e46
Matheus <matheus.hpp135@gmail.com>
2024-02-20 01:43:31 -0300

    feat: 🎸 installing and implementing pug

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+// set views as template files directory
+app.set('views', path.join(rootDir, 'views'))
+// set template engine as pug
+app.set('view engine', 'pug')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(rootDir, 'public')))
 
@@ -14,7 +19,7 @@ app.use('/admin', adminData.router)
 app.use(shopRoutes)
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render(path.join(rootDir, 'views', '404.html'))
 })
 
 app.listen(3000)
