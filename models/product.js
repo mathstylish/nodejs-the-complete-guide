66e196984e82702e1a7e12350f7ae83e04871858
Matheus <matheus.hpp135@gmail.com>
2024-02-20 17:15:01 -0300

    feat: 🎸 refactoring the file storage code

--- models/product.js
+++ models/product.js
@@ -1,19 +1,30 @@
 const path = require('../utils/path')
 const fs = require('node:fs')
 
+const storage = path.pathTo('data', 'products.json')
+
+const loadProductsFromFile = cb => {
+    // need to use arrow function here, because we need Product class context
+    fs.readFile(storage, (err, fileContent) => {
+        // need to use a callback, because readFile is async
+        // so, fetchAll returns nothing before readFile is done
+        // so, a solution is to pass this callback when readFile is done 
+        // and pass empty array if err or return fileContent
+        if (err) {
+            cb([])
+        } else {
+            cb(JSON.parse(fileContent))
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title
     }
 
     save() {
-        const storage = path.pathTo('data', 'products.json')
-        // need to use arrow function here, because we need Product context
-        fs.readFile(storage, (err, fileContent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
+        loadProductsFromFile(products => {
             products.push(this)
             fs.writeFile(storage, JSON.stringify(products), err => {
                 if (err) {
@@ -24,17 +35,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const storage = path.pathTo('data', 'products.json')
-        fs.readFile(storage, (err, fileContent) => {
-            // need to use a callback, because readFile is async
-            // so, fetchAll returns nothing before readFile is done
-            // so, a solution is to pass this callback when readFile is done 
-            // and pass empty array if err or return fileContent
-            if (err) {
-                cb([])
-            } else {
-                cb(JSON.parse(fileContent))
-            }
-        })
+        loadProductsFromFile(cb)
     }
 }
