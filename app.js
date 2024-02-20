2a8c7e7000a2cf7601629f3f7a883599129db146
Matheus <matheus.hpp135@gmail.com>
2024-02-20 00:44:49 -0300

    feat: 🎸 handling different routes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const http = require('http')
-
 const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middleware 1!')
-    next() // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This always runs!')
+    next()
 })
 
-app.use((req, res) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In middleware');
+    res.send('<h1>the "Add Product" Page</h1>')
+    next() // this will call next callback function in this app.use
+}, () => console.log("I'm done"))
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware')
-    // ...
+    res.send('<h1>Hello from Express!</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000)
-
+app.listen(3000)
