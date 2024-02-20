22e66fc241f9dac82fc9ea10dc25ca5f341e14e3
Matheus <matheus.hpp135@gmail.com>
2024-02-20 00:52:25 -0300

    feat: 🎸 limiting middleware execution to POST requests

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require('express')
-// const bodyParser = require('body-parser')
 
 const app = express()
 
-// true: use qs to parse URL-encoded data (recommended)
-// false: use querystring to parse URL-encoded data (deprecated)
-
-// in new versions of express, we can use express.urlencoded
 app.use(express.urlencoded({ extended: true }))
-
-// app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/add-product', (req, res) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use('/product', (req, res) => {
+// this will only be triggered in post requests
+app.post('/product', (req, res) => {
     console.log(req.body);
     res.redirect('/')
 })
