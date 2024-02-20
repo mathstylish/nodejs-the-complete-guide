16c616e7ec121f9c5e3df14c3d7b4c5b1286677e
Matheus <matheus.hpp135@gmail.com>
2024-02-20 01:03:01 -0300

    feat: 🎸 adding a 404 error page

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
 app.listen(3000)
