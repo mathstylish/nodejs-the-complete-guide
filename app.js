59fbf480c348a385656c34eee63c2a22b76b94f4
Matheus <matheus.hpp135@gmail.com>
2024-02-20 01:16:10 -0300

    feat: 🎸 returning 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('node:path')
 const express = require('express')
 
 const adminRoutes = require('./routes/admin')
@@ -13,7 +14,7 @@ app.use('admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
