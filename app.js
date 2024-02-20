45c662e06099e753b6589dde6a57a1fbc77a78ef
Matheus <matheus.hpp135@gmail.com>
2024-02-20 00:23:01 -0300

    feat: 🎸 sending responses
    Note: Calling the writable.end() method signals that no more data will
    be written to the Writable. In that case, res.end()

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const http = require('http')
 
-const someReqInfo = (req, res) => {
-    let { headers: { host, accept }, method, statusCode, statusMessage } = req
-    statusCode = 200
-    statusMessage = 'Fetching some request fields'
-    const userAgent = req.headers['user-agent']
-    console.log({
-        host,
-        userAgent,
-        accept,
-        method,
-        statusCode,
-        statusMessage
-    })
-}
-
-const server = http.createServer(someReqInfo)
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+  res.write('</html>')
+  res.end()
+})
 
 server.listen(3000)
