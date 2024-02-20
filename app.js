b806f62ae47004fb8de2d2c3cf5dd958cd8107a1
Matheus <matheus.hpp135@gmail.com>
2024-02-20 00:18:44 -0300

    feat: 🎸 understanding requests

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  console.log(`request: ${req}`)
-})
+const someReqInfo = (req, res) => {
+    let { headers: { host, accept }, method, statusCode, statusMessage } = req
+    statusCode = 200
+    statusMessage = 'Fetching some request fields'
+    const userAgent = req.headers['user-agent']
+    console.log({
+        host,
+        userAgent,
+        accept,
+        method,
+        statusCode,
+        statusMessage
+    })
+}
+
+const server = http.createServer(someReqInfo)
 
 server.listen(3000)
