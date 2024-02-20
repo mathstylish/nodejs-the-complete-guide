2472393e18a64f1e150fb2697e1a2611d30588d3
Matheus <matheus.hpp135@gmail.com>
2024-02-20 02:21:21 -0300

    feat: 🎸 working with handlebars
    handlebars is another template engine, like pug and ejs

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const path = require('node:path')
 const rootDir = require('./utils/path')
 const express = require('express')
 
+const handlebars = require('express-handlebars')
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-// set views as template files directory
-app.set('views', path.join(rootDir, 'views', 'pug'))
-// set template engine as pug
-app.set('view engine', 'pug')
+// plug handlebars as express engine. Pug integration comes with express, handlebars not
+app.engine('hbs', handlebars.engine({ extname: 'hbs', defaultLayout: false }))
+// set template engine as handlebars
+app.set('view engine', 'hbs')
+// set views/handlebars as template files directory
+app.set('views', path.join(rootDir, 'views', 'handlebars'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(rootDir, 'public')))
