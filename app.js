dbf5d4beddb2689d3c814abd7114acfccd7de517
Matheus <matheus.hpp135@gmail.com>
2024-02-20 03:07:08 -0300

    feat: 🎸 finishing the controllers
    create a error controller to handling not found page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rootDir = require('./utils/path')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const errorController = require('./controllers/error')
 
 const app = express()
 
@@ -17,8 +18,6 @@ app.use(express.static(path.join(rootDir, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res) => {
-    res.status(404).render('404', { pageTitle: 'Not Found', styles: ['404'] })
-})
+app.use(errorController.get404)
 
 app.listen(3000)
---
+++ controllers/error.js
@@ -0,0 +1,5 @@
+module.exports = {
+    get404: (req, res) => {
+        res.status(404).render('404', { pageTitle: 'Not Found', styles: ['404'] })
+    }
+}
