1705eed655188e681d82c425e6b45d5c3c544884
Matheus <matheus.hpp135@gmail.com>
2024-02-20 02:15:00 -0300

    feat: 🎸 converting html files to pug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoutes = require('./routes/shop')
 const app = express()
 
 // set views as template files directory
-app.set('views', path.join(rootDir, 'views'))
+app.set('views', path.join(rootDir, 'views', 'pug'))
 // set template engine as pug
 app.set('view engine', 'pug')
 
@@ -19,7 +19,7 @@ app.use('/admin', adminData.router)
 app.use(shopRoutes)
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: 'Not Found' })
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,3 @@
-const path = require('node:path')
-const rootDir = require('../utils/path')
 const express = require('express')
 
 const router = express.Router()
@@ -8,7 +6,7 @@ const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 })
 
 // /admin/add-product => POST
