d806d309956bc04badfbfb807a8f19e6ddf4ebdb
Matheus <matheus.hpp135@gmail.com>
2024-02-20 01:12:55 -0300

    feat: 🎸 serving html pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('node:path')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>')
+    res.sendFile(path.join(path.join(__dirname, '..', 'views', 'shop.html')))
 })
 
 module.exports = router;
