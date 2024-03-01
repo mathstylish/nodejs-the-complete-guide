30fe713006809d53cc3c0c819956173a34db14ba
Matheus <matheus.hpp135@gmail.com>
2024-03-01 13:02:46 -0300

    refactor: 💡 callback code to fs/promises with async await

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require('express')
 
-const $env = require('./config/env')
-
 const path = require('./utils/path')
 
 const adminRoutes = require('./routes/admin.route.js')
 const shopRoutes = require('./routes/shop.route.js')
 const errorController = require('./controllers/error.controller.js')
 
-const db = require('./database/connection')
-
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.pathTo('views'))
-
-db.execute('SELECT * FROM products')
-    .then(console.log)
-    .catch(err => {
-        if (err instanceof Error) {
-            console.log(`An error was occurred: ${err}`)
-        }
-    })
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.pathTo('public')))
@@ -31,4 +19,4 @@ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-app.listen($env.APP_PORT)
+app.listen(3000)
