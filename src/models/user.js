3b72c6e84fdba473a58d24f88c6434bf691294cb
Matheus <matheus.hpp135@gmail.com>
2024-03-03 01:27:04 -0300

    feat: 🎸 creating a user model

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/sequelize.config')
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'user'
+})
+
+module.exports = User
